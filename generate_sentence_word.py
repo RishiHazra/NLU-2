14b00541390ab6b3b0c4af71406d66af4218c50d
RishiHazra <rishixtreme@gmail.com>
2018-03-24 20:43:52 +0530

    initial commit

---
+++ generate_sentence_word.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 23:24:01 2018
+
+@author: Rishi
+"""
+
+"""
+generate sentence
+"""
+import argparse
+import json
+import pickle
+import nltk
+from word_lstm_model import WordLSTM
+
+
+def process_text(raw_text):
+    table = str.maketrans('', '', '→îé*&`%_@~è>')
+    raw_text = raw_text.translate(table)
+    raw_text
+    words=nltk.word_tokenize(raw_text)
+    return words
+
+
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--save_dir', type=str, default='./saveWord', help='Directory to save model checkpoints')
+parser.add_argument('--start', type=str, default='a', help='Start of the generation')
+parser.add_argument('--predict', type=int, default=10, help='No of predictions')
+args = parser.parse_args()
+
+with open('args_word.pckl','rb') as infile:
+    model_args=pickle.load(infile)
+    infile.close()
+
+with open('words.json','r') as infile:
+    words=json.load(infile)
+    infile.close()
+    
+model = WordLSTM(model_args,training=False)
+print(" ".join(model.generate(words,process_text(args.start), args.predict)))
+
+
+
